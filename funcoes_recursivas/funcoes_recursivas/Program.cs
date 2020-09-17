a981e4b9c858c4b5ab96d1f1803ab36c57cfc6ba
Bruno Sajermann <contato@sajermann.com>
2020-09-16 21:14:22 -0300

    Video 7 Complete

---
+++ funcoes_recursivas/funcoes_recursivas/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.IO;
+
+namespace funcoes_recursivas
+{
+    class Program
+    {
+
+        private static void LerArquivo(int numeroArquivo)
+        {
+            string caminhoRelativo = @"E:\Programação\Github\CursoTorneseUmProgramador\funcoes_recursivas\funcoes_recursivas\arquivos\arq" + numeroArquivo + ".txt";
+            if (File.Exists(caminhoRelativo))
+            {
+                using (StreamReader arquivo = File.OpenText(caminhoRelativo))
+                {
+                    string linha;
+                    while ((linha = arquivo.ReadLine()) != null)
+                    {
+                        Console.WriteLine(linha);
+                    }
+                }
+            }
+            string caminhoRelativo2 = @"E:\Programação\Github\CursoTorneseUmProgramador\funcoes_recursivas\funcoes_recursivas\arquivos\arq" + (numeroArquivo + 1) + ".txt";
+            if (File.Exists(caminhoRelativo))
+            {
+                LerArquivo(numeroArquivo + 1);
+            }
+        }
+        static void Main(string[] args)
+        {
+            LerArquivo(1);
+            Console.Read();
+        }
+    }
+}
