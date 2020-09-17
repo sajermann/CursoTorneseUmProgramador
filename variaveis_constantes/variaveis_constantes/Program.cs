3b98da3e80d76b11373042c2078f8ecece4fab0f
Bruno Sajermann <contato@sajermann.com>
2020-09-16 21:30:04 -0300

    Video 8 Complete

---
+++ variaveis_constantes/variaveis_constantes/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace variaveis_constantes
+{
+    class Program
+    {
+
+        public const int SAIDA_PROGRAMA = 1;
+
+        static void Main(string[] args)
+        {
+            while (true)
+            {
+                Console.WriteLine("Digite " + SAIDA_PROGRAMA + " para sair do programa");
+                int valor = Convert.ToInt32(Console.ReadLine());
+                if(SAIDA_PROGRAMA == valor)
+                {
+                    break;
+                }
+            }
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
