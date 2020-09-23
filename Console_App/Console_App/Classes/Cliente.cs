c45e97c4bf0051ca0aee65de2bf0cbec6965a1e2
Bruno Sajermann <contato@sajermann.com>
2020-09-22 21:54:55 -0300

    Video 12 Complete

--- Console_App/Console_App/Classes/Cliente.cs
+++ Console_App/Console_App/Classes/Cliente.cs
@@ -8,13 +8,75 @@ namespace Classes
 {
     public class Cliente
     {
+        /// <summary>
+        /// Construtor com três parâmetros
+        /// </summary>
+        /// <param name="nome"></param>
+        /// <param name="telefone"></param>
+        /// <param name="cpf"></param>
+        public Cliente(string nome, string telefone, string cpf)
+        {
+            this.Nome = nome;
+            this.Telefone = telefone;
+            this.Cpf = cpf;
+        }
+
+        /// <summary>
+        /// Construtor da Class
+        /// </summary>
+        /// <param name="nome">Preencher o nome do objeto</param>
+        public Cliente(string nome)
+        {
+            this.Nome = nome;
+        }
+
+        /// <summary>
+        /// Construtor com parâmetro telefone inteiro
+        /// </summary>
+        /// <param name="telefone">Telefone Inteiro</param>
+        public Cliente(int telefone)
+        {
+            this.Telefone = telefone.ToString();
+        }
+        /// <summary>
+        /// Construtor sem parâmetros
+        /// </summary>
+        public Cliente()
+        {
+            
+        }
+
+        public static string Teste;
+
         public string Nome;
         public string Telefone;
         public string Cpf;
 
         public void Gravar()
         {
-            //Todo
+            var clientes = Cliente.LerClientes();
+
+            clientes.Add(this);
+
+            if (File.Exists(caminhoBaseClientes()))
+            {
+                //string conteudo = "nome;telefone;cpf;\n";
+                //foreach (Cliente c in clientes)
+                //{
+                //    conteudo += c.Nome + ";" + c.Telefone + ";" + c.Cpf + ";\n";
+                //}
+                //File.WriteAllText(caminhoBaseClientes(), conteudo);
+
+                //Metódo 2 para gravar
+                StreamWriter r = new StreamWriter(caminhoBaseClientes());
+                r.WriteLine("nome;telefone;cpf;");
+                foreach (Cliente c in clientes)
+                {
+                    var linha = c.Nome + ";" + c.Telefone + ";" + c.Cpf + ";";
+                    r.WriteLine(linha);
+                }
+                r.Close();
+            }
         }
         private static string caminhoBaseClientes()
         {
@@ -23,7 +85,6 @@ namespace Classes
 
         public static List<Cliente> LerClientes()
         {
-
             var clientes = new List<Cliente>();
             if (File.Exists(caminhoBaseClientes()))
             {
@@ -36,11 +97,12 @@ namespace Classes
                         i++;
                         if (i == 1) continue;
                         var clienteArquivo = linha.Split(';');
-                        var cliente = new Cliente();
-                        cliente.Nome = clienteArquivo[0];
-                        cliente.Telefone = clienteArquivo[1];
-                        cliente.Cpf = clienteArquivo[2];
-
+                        //var cliente = new Cliente(clienteArquivo[0],clienteArquivo[1],clienteArquivo[2]);
+                        var cliente = new Cliente {
+                            Nome = clienteArquivo[0],
+                            Telefone = clienteArquivo[1],
+                            Cpf = clienteArquivo[2]
+                        };
                         clientes.Add(cliente);
                     }
                 }
