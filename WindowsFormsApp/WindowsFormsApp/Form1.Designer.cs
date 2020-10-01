c04223ac782edb4f610c6d85b34deff0ac2454fb
Bruno Sajermann <contato@sajermann.com>
2020-09-30 22:15:59 -0300

    Video 23 Complete

---
+++ WindowsFormsApp/WindowsFormsApp/Form1.Designer.cs
@@ -0,0 +1,87 @@
+﻿namespace WindowsFormsApp
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.TBResultado = new System.Windows.Forms.TextBox();
+            this.BTOk = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 16F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(150, 19);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(466, 26);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Pressione o ok para preencher a caixa de texto";
+            // 
+            // TBResultado
+            // 
+            this.TBResultado.Location = new System.Drawing.Point(155, 48);
+            this.TBResultado.Name = "TBResultado";
+            this.TBResultado.Size = new System.Drawing.Size(461, 20);
+            this.TBResultado.TabIndex = 1;
+            // 
+            // BTOk
+            // 
+            this.BTOk.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.BTOk.Location = new System.Drawing.Point(327, 74);
+            this.BTOk.Name = "BTOk";
+            this.BTOk.Size = new System.Drawing.Size(138, 61);
+            this.BTOk.TabIndex = 2;
+            this.BTOk.Text = "OK";
+            this.BTOk.UseVisualStyleBackColor = true;
+            this.BTOk.Click += new System.EventHandler(this.BTOk_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.BTOk);
+            this.Controls.Add(this.TBResultado);
+            this.Controls.Add(this.label1);
+            this.Font = new System.Drawing.Font("Microsoft Sans Serif", 8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Name = "Form1";
+            this.Text = "Aplicação 1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox TBResultado;
+        private System.Windows.Forms.Button BTOk;
+    }
+}
+
